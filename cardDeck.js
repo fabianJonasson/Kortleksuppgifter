d04e16c55e0a7a412188170970824f90959dcb3d
fabianJonasson <fabian.jonasson@kuberacad.se>
2023-09-20 12:06:44 +0200

    added function to deal a specified amount of cards to a specified player

--- cardDeck.js
+++ cardDeck.js
@@ -2,12 +2,19 @@ const value = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 const suite = ["hearts", "diamonds", "spades", "clubs"];
 
 let deck = [];
+let player1Deck = [];
+let player2Deck = [];
+
 suite.forEach(color => {
     value.forEach(num => {
         deck.push(`${num} of ${color}`); 
     })
 });
 
+deck = shuffleDeck();
+
+dealCards(player1Deck, 5);
+dealCards(player2Deck, 5);
 
 function shuffleDeck() {
     let shuffledDeck = [];
@@ -19,4 +26,9 @@ function shuffleDeck() {
     return shuffledDeck;   
 }
 
-deck = shuffleDeck();
+function dealCards(player, number) {
+    for(let i = 0; i < number; i++) {
+        player.push(deck[i]);
+        deck.splice(i, 1);
+    }
+}
