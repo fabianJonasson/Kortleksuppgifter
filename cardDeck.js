80f2afbfa18ab8c6cf58361bb5666c64f7122372
fabianJonasson <fabian.jonasson@kuberacad.se>
2023-09-20 11:08:22 +0200

    added two loops that populates the deck-array with cards

--- cardDeck.js
+++ cardDeck.js
@@ -2,3 +2,8 @@ const value = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 const suite = ["hearts", "diamonds", "spades", "clubs"];
 
 let deck = [];
+suite.forEach(color => {
+    value.forEach(num => {
+        deck.push(`${num} of ${color}`); 
+    })
+});
